7d4466271517e8ad67f98263e965ce78db03a9ae
Ardanto Finkan Septa <ardantofinsep14@gmail.com>
2019-04-29 23:02:28 +0700

    fix impro

--- src/gabut/src/second_mission.cpp
+++ src/gabut/src/second_mission.cpp
@@ -1,29 +1,177 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
 #include "../include/gabut/hoho.hpp"
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
 #include <cv_bridge/cv_bridge.h>
-#include <iostream>
 #include <stdio.h>
 #include "gabut/image_value.h"
-#include <string>
-#include <sstream>
 #include <cstdlib>
 #include <sensor_msgs/Joy.h>
-
-using namespace std;
 using namespace cv;
+using namespace std;
 
-Mat rov_image, mini_image;
 bool refreshed = true;
 
-void secondMissionProcessing(Mat input_image);
-void thirdMissionProcessing(Mat input_image);
-void miniProcessing(Mat input_image);
+Mat Original;
 
+Mat rov_image, mini_image;
 gabut::image_value image;
 ros::Publisher pub_state_camera;
+
+//convert double to string
+namespace patch
+{
+    template<typename T> std::string to_string(const T& n)
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+static double angle(Point pt1, Point pt2, Point pt0)
+{
+	double dx1 = pt1.x - pt0.x;
+	double dy1 = pt1.y - pt0.y;
+	double dx2 = pt2.x - pt0.x;
+	double dy2 = pt2.y - pt0.y;
+	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+/**
+ * Helper function to display text in the center of a contour
+ */
+void setLabel(Mat& im, const std::string label, std::vector<Point>& contour)
+{
+	int fontface = FONT_HERSHEY_SIMPLEX;
+	double scale = 0.4;
+	int thickness = 1;
+	int baseline = 0;
+
+	Size text = getTextSize(label, fontface, scale, thickness, &baseline);
+	Rect r = boundingRect(contour);
+
+	Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+	rectangle(im, pt + Point(0, baseline), pt + Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
+	putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+}
+
+void ngode(Mat Original)
+{		
+	int countA = 0;
+	int countB = 0;
+	int countC = 0;
+	int countD = 0;
+	Mat gray,bw,gray2;
+	Mat dst = Original.clone();
+	erode(dst, dst, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+	// GaussianBlur(Original, Original, Size(5, 5), 2, 2);
+	cvtColor(dst, gray, COLOR_BGR2GRAY);
+	threshold( gray, bw,0, 115,3 );
+	Canny(bw, gray2, 0, 50, 5);
+	vector<vector<Point> > contours;
+	findContours(gray2.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	vector<Point> approx;
+
+    for (int i = 0; i < contours.size(); i++)
+    {
+	    // Approximate contour with accuracy proportional
+	    // to the contour perimeter
+	    approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+	    // Skip small or non-convex objects 
+	    if (std::fabs(contourArea(contours[i])) < 1000 || !isContourConvex(approx))
+		    continue;
+
+	    if (approx.size() == 3)
+	    {
+	    	/**
+		    if (std::fabs(contourArea(contours[i])) < 10000 || !isContourConvex(approx))
+		    {
+			    continue;
+			
+		    }
+		    */
+		    setLabel(dst, "TRI", contours[i]);
+		    countA++;
+
+	    }
+	     else if (approx.size() >= 4 && approx.size() <= 6)
+	    {
+		    // Number of vertices of polygonal curve
+		    int vtc = approx.size();
+
+		    // Get the cosines of all corners
+		    std::vector<double> cos;
+		    for (int j = 2; j < vtc+1; j++)
+			    cos.push_back(angle(approx[j%vtc], approx[j-2], approx[j-1]));
+
+		    // Sort ascending the cosine values
+		    std::sort(cos.begin(), cos.end());
+
+		    // Get the lowest and the highest cosine
+		    double mincos = cos.front();
+		    double maxcos = cos.back();
+
+		    // Use the degrees obtained above and the number of vertices
+		    // to determine the shape of the contour
+		    if (vtc == 4)// && mincos >= -0.1 && maxcos <= 0.3)
+			    {
+			    double area = contourArea(contours[i]);
+			    double keliling = arcLength( contours[i], true );
+			    double ganteng = (keliling*keliling)/(16*area);
+			
+			    if (ganteng >= 0.8 and ganteng <= 1.15)
+				    {	
+				    setLabel(dst, "SQR", contours[i]);
+				    countB++;
+				    }
+			    else 
+			    {
+				    setLabel(dst, "RECT", contours[i]);
+				    countC++;
+			    }
+			 }
+		    else if (vtc == 5)// && mincos >= -0.34 && maxcos <= -0.27)
+			    setLabel(dst, "PENTA", contours[i]);
+		    else if (vtc == 6)// && mincos >= -0.55 && maxcos <= -0.45)
+			    setLabel(dst, "HEXA", contours[i]);
+	    }
+	    else
+	    {
+		    // Detect and label circles
+		    double area = contourArea(contours[i]);
+		    Rect r = boundingRect(contours[i]);
+		    int radius = r.width / 2;
+
+		    if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
+		        std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
+			     { setLabel(dst, "CIR", contours[i]); countD++; }
+
+	    }
+    }
+    stringstream ss;
+    ss << countA;
+    putText(dst, ss.str(), Point(550, 100), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 255), 1);
+    ss.str("");
+    ss << countB;
+    putText(dst, ss.str(), Point(550, 200), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 255), 1);
+    ss.str("");
+    ss << countC;
+    putText(dst, ss.str(), Point(550, 300), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 255), 1);
+    ss.str("");
+    ss << countD;
+    putText(dst, ss.str(), Point(550, 400), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 255), 1);
+	
+	imshow("Circle Detector", dst);
+	waitKey(30);
+}
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
 	int a = joy->buttons[9];
@@ -41,7 +189,7 @@ void rovCallback(const sensor_msgs::CompressedImageConstPtr& msg)
     	refreshed = false;
 	}
     waitKey(10);
-    secondMissionProcessing(rov_image);
+    ngode(rov_image);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -61,6 +209,8 @@ int main(int argc, char **argv){
 	ros::Subscriber joy_sub = nh.subscribe("joy", 8, &joyCallback);
 	
 	namedWindow("panel", CV_WINDOW_AUTOSIZE);
+	
+	namedWindow("Circle Detector", CV_WINDOW_AUTOSIZE);
 
 	createTrackbar("LowH_black", "panel", &LowH_black, 255);
 	createTrackbar("HighH_black", "panel", &HighH_black, 255);
@@ -80,126 +230,3 @@ int main(int argc, char **argv){
 	}
 }
 
-void secondMissionProcessing(Mat input_image) {
-	Mat Original = input_image.clone();
-	Mat imgHSV, allColour, allContour, Threshold, BW;
-	int Index;
-	imgHSV = Original;
-
-	// cvtColor(Original, imgHSV, COLOR_BGR2HSV);
-
-	/**
-	erode(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	dilate( imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
-	dilate( imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-	erode(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	**/
-	inRange(imgHSV, Scalar(LowH_black, LowS_black, LowV_black), Scalar(HighH_black, HighS_black, HighV_black), Threshold); //Threshold the image
-		
-	Canny(Threshold, BW, 0, 50, 5);
-		
-	vector<vector<Point> > Contours;
-		
-	findContours(BW, Contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-			
-	vector<Point> Approx;
-	int count_triangle, count_square, count_strip, count_circle;
-	count_strip = 0;
-	count_circle = 0;
-	count_square = 0;
-	count_triangle = 0;
-	
-	for (Index = 0; Index < Contours.size(); Index++){
-		approxPolyDP(Mat(Contours[Index]), Approx, arcLength(Mat(Contours[Index]), true)*0.02, true);
-		
-		if (fabs(contourArea(Contours[Index])) < 5000 || !isContourConvex(Approx))
-		continue;
-		if (Approx.size() == 2){
-			count_strip++;
-		}
-
-		if (Approx.size() == 3){
-			count_triangle++;
-		}
-		
-		if (Approx.size() == 4){
-			count_square++;
-		}
-	}
-
-	GaussianBlur(Threshold, Threshold, Size(9, 9), 2, 2);
-	
-	vector<Vec3f> circles;
-	HoughCircles(Threshold, circles, HOUGH_GRADIENT, 1,
-		Threshold.rows/16, 
-		100, 30, min_radius_black, max_radius_black);
-		
-	for (size_t i = 0; i < circles.size(); i++ ){
-		count_circle++;
-	}
-	
-	line( Original, Point( strip_x, strip_y1 ), Point( strip_x, strip_y2), Scalar( 0, 255, 255 ), 5, 8 );	
-	
-	circle( Original, Point(circle_x, circle_y), 30, Scalar( 0, 255, 255 ), 2);
-	
-	rectangle(Original,Rect(square_x,square_y,50,50),Scalar(0,255,255),3,8,0);
-	
-	line( Original, Point( tri_x1, tri_y1 ), Point( tri_x2, tri_y2), Scalar( 0, 255, 255 ), 2, 8 );
-	line( Original, Point( tri_x2, tri_y2 ), Point( tri_x3, tri_y3), Scalar( 0, 255, 255 ), 2, 8 );	
-	line( Original, Point( tri_x3, tri_y3 ), Point( tri_x1, tri_y1), Scalar( 0, 255, 255 ), 2, 8 );	
-	
-	//cout<<count_circle<<endl;
-	stringstream ss;
-	ss << count_square;
-	square_text = ss.str();
-
-	ss.str("");
-	ss << count_strip;
-	strip_text = ss.str();
-	
-	ss.str("");
-	ss << count_triangle;
-	tri_text = ss.str();
-
-	ss.str("");
-	ss << count_circle;
-	circle_text = ss.str();
-
-	string ph_text_value;
-	string heat_text_value;
-
-	float randnum = rand() % 5 + 75;
-	float randnumfloat = randnum / 10.0;
-	ss.str("");
-	ss << randnumfloat;
-	ph_text_value = ss.str();
-
-	randnum = rand() % 5 + 30;
-	ss.str("");
-	ss << randnum;
-	heat_text_value = ss.str();
-	
-	putText(Original, strip_text, Point(strip_x_text, strip_y_text), FONT_HERSHEY_DUPLEX, 2, Scalar(0,255,255), 2);
-	putText(Original, circle_text, Point(circle_x_text, circle_y_text), FONT_HERSHEY_DUPLEX, 2, Scalar(0,255,255), 2);
-	putText(Original, square_text, Point(square_x_text, square_y_text), FONT_HERSHEY_DUPLEX, 2, Scalar(0,255,255), 2);
-	putText(Original, tri_text, Point(tri_x_text, tri_y_text), FONT_HERSHEY_DUPLEX, 2, Scalar(0,255,255), 2);
-
-	putText(Original, ph_text, Point(ph_x, ph_y), FONT_HERSHEY_DUPLEX, 1, Scalar(0,255,255), 2);
-	putText(Original, ph_text_value, Point(ph_x_value, ph_y_value), FONT_HERSHEY_DUPLEX, 1, Scalar(0,255,255), 2);
-	putText(Original, heat_text, Point(heat_x, heat_y), FONT_HERSHEY_DUPLEX, 1, Scalar(0,255,255), 2);
-	putText(Original, heat_text_value, Point(heat_x_value, heat_y_value), FONT_HERSHEY_DUPLEX, 1, Scalar(0,255,255), 2);
-	
-	//imshow("second colour", Threshold);
-	//imshow("second contour", BW);
-	imshow("second", Original);
-	imshow("thresh", Threshold);
-}
-
-void thirdMissionProcessing(Mat input_image){
-	imshow("third", input_image);
-}
-
-void miniProcessing(Mat input_image){
-	imshow("mini", input_image);
-}
