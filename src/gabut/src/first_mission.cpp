a0bbd176f4ceabf4b4370b16b49682ec795e544d
Ardanto Finkan Septa <ardantofinsep14@gmail.com>
2019-05-05 09:16:01 +0700

    amv juara 1

--- src/gabut/src/first_mission.cpp
+++ src/gabut/src/first_mission.cpp
@@ -14,8 +14,6 @@ using namespace cv;
 
 Mat rov_image, mini_image;
 
-void miniProcessing(Mat input_image);
-
 gabut::image_value image;
 ros::Publisher pub_state_camera;
 
@@ -34,20 +32,6 @@ void rovCallback(const sensor_msgs::CompressedImageConstPtr& msg)
   }
 }
 
-void miniCallback(const sensor_msgs::CompressedImageConstPtr& msg)
-{
-  try
-  {
-    mini_image = cv::imdecode(cv::Mat(msg->data),1);//convert compressed image data to cv::Mat
-    waitKey(10);
-    miniProcessing(mini_image);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert to image!");
-  }
-}
-
 int main(int argc, char **argv){
 	ros::init(argc, argv, "videoRec");
 	ros::NodeHandle nh;
@@ -57,36 +41,8 @@ int main(int argc, char **argv){
 	
 	pub_state_camera 			= nh.advertise<gabut::image_value>("/mate/value/image", 1);
 	ros::Subscriber sub_rov 	= nh.subscribe("/mate/image/rov/compressed", 1, rovCallback);
-	ros::Subscriber sub_mini 	= nh.subscribe("/mate/image/mini/compressed", 1, miniCallback);
-	
-	namedWindow("panel", CV_WINDOW_AUTOSIZE);
-	
-	createTrackbar("LowH_red", "panel", &LowH_red, 255);
-	createTrackbar("HighH_red", "panel", &HighH_red, 255);
-	createTrackbar("LowS_red", "panel", &LowS_red, 255); 
-	createTrackbar("HighS_red", "panel", &HighS_red, 255);
-	createTrackbar("LowV_red", "panel", &LowV_red, 255);
-	createTrackbar("HighV_red", "panel", &HighV_red, 255);
-	createTrackbar("noise_red", "panel", &Noise_red, 255);
-
-	createTrackbar("LowH_blue", "panel", &LowH_blue, 255);
-	createTrackbar("HighH_blue", "panel", &HighH_blue, 255);
-	createTrackbar("LowS_blue", "panel", &LowS_blue, 255); 
-	createTrackbar("HighS_blue", "panel", &HighS_blue, 255);
-	createTrackbar("LowV_blue", "panel", &LowV_blue, 255);
-	createTrackbar("HighV_blue", "panel", &HighV_blue, 255);
-	createTrackbar("noise_blue", "panel", &Noise_blue, 255);
-
-	createTrackbar("x", "panel", &x_init, 700); //Hue (0 - 255)
-	createTrackbar("y", "panel", &y_init, 700);
-	createTrackbar("width", "panel", &width, 700); //Saturation (0 - 255)
-	createTrackbar("hight", "panel", &height, 700);
 	
 	while (ros::ok()) {
 		ros::spinOnce();
 	}
-}
-
-void miniProcessing(Mat input_image){
-	imshow("mini", input_image);
 }
